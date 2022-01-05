8ad0f3e68a221e6525d44af3a12414e8d0669a8d
antonio-oliva <antonio_oliva@outlook.it>
2022-01-05 19:12:53 +0100

    change example for Adapter

--- Adapter.py
+++ Adapter.py
@@ -1,53 +1,71 @@
-class Target:
+import math
+
+
+class RoundPeg:
     """
-    The Target defines the domain-specific interface used by the client code.
+    RoundPeg(s) are compatible with RoundHole(s)
+    """
+    def __init__(self, radius: float):
+        self._radius = radius
+
+    def getRadius(self) -> float:
+        return self._radius
+
+
+class RoundHole:
+    """
+    RoundHole(s) are compatible with RoundPeg(s)
+    """
+    def __init__(self, radius: float):
+        self._radius = radius
+
+    def getRadius(self) -> float:
+        return self._radius
+
+    def fits(self, peg: RoundPeg) -> bool:
+        return self.getRadius() >= peg.getRadius()
+
+
+class SquarePeg:
+    """
+    SquarePeg(s) are not compatible with RoundHole(s).
+    But we need to integrate them into our program.
+    """
+    def __init__(self, width: float):
+        self._width = width
+
+    def getWidth(self) -> float:
+        return self._width
+
+
+class SquarePegAdapter:
+    """
+    The Adapter Adapter allows fitting square pegs into round holes.
     """
 
-    def request(self) -> str:
-        return "Target: The default target's behavior."
+    def __init__(self, squarePeg: SquarePeg):
+        self.squarePeg = squarePeg
 
-
-class Adaptee:
-    """
-    The Adaptee contains some useful behavior, but its interface is incompatible
-    with the existing client code. The Adaptee needs some adaptation before the
-    client code can use it.
-    """
-
-    def request(self) -> str:
-        return ".eetpadA eht fo roivaheb laicepS"
-
-
-class Adapter(Target):
-    """
-    The Adapter makes the Adaptee's interface compatible with the Target's
-    interface via multiple inheritance.
-    """
-
-    def __init__(self, adaptee: Adaptee = Adaptee()):
-        self.adaptee = adaptee
-
-    def request(self) -> str:
-        return f"Adapter: (TRANSLATED) {self.adaptee.request()[::-1]}"
-
-
-def client_code(target: Target) -> None:
-    """
-    The client code supports all classes that follow the Target interface.
-    """
-
-    print(target.request(), end="\n\n")
+    def getRadius(self) -> float:
+        return self.squarePeg.getWidth() * math.sqrt(2) / 2
 
 
 if __name__ == "__main__":
-    print("Client: I can work just fine with the Target objects:")
-    target = Target()
-    client_code(target)
+    hole = RoundHole(5)
 
-    adaptee = Adaptee()
-    print("Client: The Adaptee class has a weird interface. See, I don't understand it:")
-    print(f"Adaptee: {adaptee.request()}", end="\n\n")
+    # Round fits round, no surprise.
+    rpeg = RoundPeg(5)
+    if hole.fits(rpeg):
+        print("Round peg r5 fits round hole r5!")
 
-    print("Client: But I can work with it via the Adapter:")
-    adapter = Adapter()
-    client_code(adapter)
+    smallSqPeg = SquarePeg(2)
+    largeSqPeg = SquarePeg(20)
+    # hole.fits(smallSqPeg)  # Won't work!
+
+    # Adapter solves the problem.
+    smallSqPegAdapter = SquarePegAdapter(smallSqPeg)
+    largeSqPegAdapter = SquarePegAdapter(largeSqPeg)
+    if hole.fits(smallSqPegAdapter):
+        print("Square peg w2 fits round hole r5!")
+    if not hole.fits(largeSqPegAdapter):
+        print("Square peg w20 does not fit into round hole r5!")
